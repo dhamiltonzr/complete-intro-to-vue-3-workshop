e8e332a3a9aeba55f429b1c7647ede19b5612312
Dean M. Hamilton <dean@zaneray.com>
2023-10-01 11:21:27 -0600

    Create components with props

---
+++ cougs-forum/src/components/FavoritePlayerPercentage.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>{{ playerTitle }} - {{ favoritePlayerPercentage }}% Favorites</h2>
+</template>
+<script>
+export default {
+  props: {
+    playerList: {
+      type: Array,
+      required: true
+    },
+    playerTitle: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    favoritePlayerPercentage() {
+      return Math.floor((this.getFavorites(this.playerList).length / this.playerList.length) * 100);
+    }
+  },
+  methods: {
+    getFavorites(playerList) {
+      return playerList.filter(function (player) {
+        return player.favorite
+      });
+    }
+  }
+}
+</script>
---
+++ cougs-forum/src/components/FavoritePlayers.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <h2>{{ playerTitle }}</h2>
+  <ul>
+    <li v-for="(player, index) in favoritePlayerList" :key="`fav-${this.playerTitle}-${index}`">
+      {{ player.name }} - NFL team: {{ player.nflTeam }}
+    </li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  props: {
+    playerList: {
+      type: Array,
+      required: true
+    },
+    playerTitle: {
+      type: String,
+      required: true
+    },
+    playerType: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    favoritePlayerList() {
+      return this.getFavorites(this.playerList);
+    }
+  },
+  methods: {
+    getFavorites(playerList) {
+      return playerList.filter(function (player) {
+        return player.favorite
+      });
+    }
+  }
+}
+</script>
